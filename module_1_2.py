de34d6328e9796ffea8cd935f54552411a4a45b5
ovzovz <ovz.ovz@ya.ru>
2024-08-01 13:26:10 +0300

    Add files via upload

---
+++ module_1_2.py
@@ -0,0 +1,5 @@
+task_count=12
+hour_count=1.5
+course_name='Python'
+time_4_1_task=hour_count/task_count
+print("Курс:",course_name,", всего задач:",task_count,", затрачено часов:",hour_count,",  среднее время выполнения ",time_4_1_task)
