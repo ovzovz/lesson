ed7bcea05c757da611d645a75687ca0cd8142040
ovzovz <ovz.ovz@ya.ru>
2024-08-12 19:40:39 +0300

    Update module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -9,7 +9,7 @@ def send_email(message, recipient, *, sender="university.help@gmail.com"):
         print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}.')
     else:
         print(f'"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.')
-    return
+    
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 print()
