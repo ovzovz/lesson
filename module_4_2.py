238086bbc4c7003b34c4d34eefe757d8a140d291
ovzovz <ovz.ovz@ya.ru>
2024-08-25 22:27:30 +0300

    Add files via upload

---
+++ module_4_2.py
@@ -0,0 +1,14 @@
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+
+    inner_function()
+
+test_function() #Я в области видимости функции test_function
+inner_function() #NameError: name 'inner_function' is not defined. Did you mean: 'test_function'?
+
+
+
+
+
+
