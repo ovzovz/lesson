b5a5abdff13abc989cda3c924de4b0a551f27d3c
ovzovz <ovz.ovz@ya.ru>
2024-11-27 21:27:39 +0300

    Add files via upload

--- crud_functions.py
+++ crud_functions.py
@@ -9,6 +9,26 @@ def initiate_db():
                "description TEXT,"
                "price INTEGER NOT NULL)")
     connection.commit()
+    cursor.execute("CREATE TABLE IF NOT EXISTS Users("
+               "id INTEGER PRIMARY KEY,"
+               "username TEXT NOT NULL,"
+               "email TEXT NOT NULL,"
+               "age INTEGER NOT NULL,"
+               "balance INTEGER NOT NULL)")
+    connection.commit()
+
+def add_user(username, email, age):
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO Users (username, email, age, balance)  VALUES (?,?,?,1000)",(username,email,age))
+    connection.commit()
+
+def is_included(username):
+    cursor = connection.cursor()
+    check_user = cursor.execute(f'SELECT * FROM Users WHERE username="{username}"')
+    if check_user.fetchone() is None:
+        return False
+    else:
+        return True
 
 def fill_table():
     cursor = connection.cursor()
@@ -27,6 +47,9 @@ def get_all_products():
     products=cursor.execute("SELECT * FROM Products")
     return products.fetchall()
 
-#initiate_db()
-#fill_table()
-#pr=get_all_products()
+
+initiate_db()
+#if not is_included('An'):
+ #  add_user('An', 'ann@mail.ru', 25)
+
+
