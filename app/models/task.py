4fcd31b864f51eca4244cb85ea5fd5943609e908
ovzovz <ovz.ovz@ya.ru>
2025-01-01 20:13:55 +0300

    Update task.py

--- app/models/task.py
+++ app/models/task.py
@@ -13,7 +13,7 @@ class Task(Base):
     completed = Column(Boolean, default=False)
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False, index=True)
     slug = Column(String, unique=True, index=True)
-    user = relationship("User", back_populates = 'tasks')
+    users = relationship("User", back_populates = 'tasks')
 
 from sqlalchemy.schema import CreateTable
 print(CreateTable(Task.__table__))
